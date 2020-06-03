859c95ccc96f4d48ce656b85e20c17a327fa7a12
nartco <sebastien.thuillier@hotmail.com>
2020-06-03 22:47:48 +0200

    Profile action to db

--- client/src/component/profile-forms/CreateProfile.js
+++ client/src/component/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
     const [formData, setFormData]  = useState({
         company: '',
         website: '',
@@ -37,6 +39,10 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
 
+    const onSubmit = e => {
+        e.preventDefault()
+        createProfile(formData, history)
+    }
 
     return (
         <Fragment>
@@ -48,7 +54,7 @@ const CreateProfile = props => {
             profile stand out
         </p>
         <small>* = required field</small>
-        <form className="form">
+        <form className="form" onSubmit={e => onSubmit(e)}>
             <div className="form-group">
             <select name="status" value={status} onChange={(e) => onChange(e)}>
                 <option value="0">* Select Professional Status</option>
@@ -61,34 +67,26 @@ const CreateProfile = props => {
                 <option value="Intern">Intern</option>
                 <option value="Other">Other</option>
             </select>
-            <small className="form-text"
-                >Give us an idea of where you are at in your career</small
-            >
+            <small className="form-text">Give us an idea of where you are at in your career</small>
             </div>
             <div className="form-group">
             <input type="text" placeholder="Company" name="company" value={company} onChange={e => onChange(e)}/>
-            <small className="form-text"
-                >Could be your own company or one you work for</small
-            >
+            <small className="form-text">Could be your own company or one you work for</small>
             </div>
             <div className="form-group">
             <input type="text" placeholder="Website" name="website" value={website} onChange={e => onChange(e)} />
-            <small className="form-text"
-                >Could be your own or a company website</small
-            >
+            <small className="form-text">Could be your own or a company website</small>
             </div>
             <div className="form-group">
             <input type="text" placeholder="Location" name="location" value={location} onChange={e => onChange(e)} />
-            <small className="form-text"
-                >City & state suggested (eg. Boston, MA)</small
-            >
+            <small className="form-text">City & state suggested (eg. Boston, MA)</small>
             </div>
             <div className="form-group">
             <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={e => onChange(e)}/>
             <small className="form-text"
                 >Please use comma separated values (eg.
-                HTML,CSS,JavaScript,PHP)</small
-            >
+                HTML,CSS,JavaScript,PHP)
+                </small>
             </div>
             <div className="form-group">
             <input
@@ -97,10 +95,10 @@ const CreateProfile = props => {
                 name="githubusername"
                 value={githubusername} onChange={e => onChange(e)}
             />
-            <small className="form-text"
-                >If you want your latest repos and a Github link, include your
-                username</small
-            >
+            <small className="form-text">
+            If you want your latest repos and a Github link, include your
+                username
+            </small>
             </div>
             <div className="form-group">
             <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
@@ -114,7 +112,8 @@ const CreateProfile = props => {
             <span>Optional</span>
             </div>
 
-            {displaySocialInputs && <Fragment>
+            {displaySocialInputs && (
+            <Fragment>
                 <div className="form-group social-input">
                     <i className="fab fa-twitter fa-2x"></i>
                     <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={e => onChange(e)} />
@@ -139,7 +138,8 @@ const CreateProfile = props => {
                     <i className="fab fa-instagram fa-2x"></i>
                     <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={e => onChange(e)} />
                 </div>
-            </Fragment>}
+            </Fragment>
+            )}
 
             
             <input type="submit" className="btn btn-primary my-1" />
@@ -150,7 +150,8 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired
 }
 
-export default CreateProfile
+
+export default connect(null, { createProfile }) (withRouter(CreateProfile))
