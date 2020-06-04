c57e084a9fa50368c6e96ba9ee401cb6db306ddc
nartco <sebastien.thuillier@hotmail.com>
2020-06-04 18:51:17 +0200

    add education & experience

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './component/layout/Alert'
 import Dashboard from './component/dashboard/Dashboard'
 import CreateProfile from './component/profile-forms/CreateProfile'
 import EditProfile from './component/profile-forms/EditProfile'
+import AddExperience from './component/profile-forms/AddExperience'
+import AddEducation from './component/profile-forms/AddEducation'
 import PrivateRoute from './component/routing/PrivateRoute'
 import { loadUser }  from './actions/auth'
 import setAuthToken from './utils/setAuthToken'
@@ -40,6 +42,8 @@ const App = () => {
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+            <PrivateRoute exact path='/add-experience' component={AddExperience} />
+            <PrivateRoute exact path='/add-education' component={AddEducation} />
           </Switch>
         </section>
       </Fragment>
